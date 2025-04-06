44fe2cc6867e80f6971f92ccf7c570545623e2c5
Juan Manuel M <juanmuzou@gmail.com>
2025-04-06 16:14:57 -0500

    creamos la clase ubicacion y clima  con su atributo mostrar informcion

--- AppClima/clima_operativo.py
+++ AppClima/clima_operativo.py
@@ -1,3 +1,21 @@
+import requests
+
+
+class Ubicacion:
+    def __init__(self, ciudad: str, pais: str):
+        self.ciudad = ciudad
+        self.pais = pais
+
+class Clima:
+    def __init__(self, temperatura: float, humedad: int, viento: float, descripcion: str):
+        self.temperatura = temperatura
+        self.humedad = humedad
+        self.viento = viento
+        self.descripcion = descripcion
+
+    def mostrar_info(self) -> str:
+        return f"{self.temperatura}°C, {self.humedad}%, {self.viento} km/h, {self.descripcion}"
+
 
 
 
