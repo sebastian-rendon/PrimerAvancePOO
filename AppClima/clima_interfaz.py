bfeced8953c9c23d054cdbfb9662626f362a4ea2
KevinStiven <kaguirre070@soyudemedellin.edu.co>
2025-04-06 20:24:20 -0500

    Se creó la clase AplicacionClima y se configuró la ventana de interfaz

--- AppClima/clima_interfaz.py
+++ AppClima/clima_interfaz.py
@@ -1,1 +1,24 @@
+import tkinter as tk
+from clima_operativo import Ubicacion, APIClima, Usuario, HistorialClima
 
+class AplicacionClima:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Aplicación de Clima")
+        self.root.geometry("500x400")
+
+        self.historial = HistorialClima()
+
+        tk.Label(root, text="Ingrese ciudad y país:").pack()
+
+        self.entry_ciudad = tk.Entry(root)
+        self.entry_ciudad.pack()
+
+        self.entry_pais = tk.Entry(root)
+        self.entry_pais.pack()
+
+        tk.Button(root, text="Obtener Clima", command=self.obtener_clima_usuario).pack()
+        tk.Button(root, text="Ver Historial", command=self.mostrar_historial).pack()
+
+        self.texto_resultado = tk.Text(root, height=10, width=50)
+        self.texto_resultado.pack()
