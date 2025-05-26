e3f7f6efa4799260cbbe9c8039192e6dafe9e9c7
SebastianRendon <srendon521@gmail.com>
2025-05-26 10:53:19 -0500

    Definimos la clase PronosticoDia

--- AppClima/clima_operativo.py
+++ AppClima/clima_operativo.py
@@ -13,7 +13,15 @@ class Clima:
     def mostrar_info(self) -> str:
         return f"{self.temperatura}°C, {self.humedad}%, {self.viento} km/h, {self.descripcion}"
 
+class PronosticoDia:
+    def __init__(self, fecha: str, temp_min: float, temp_max: float, descripcion: str):
+        self.fecha = fecha
+        self.temp_min = temp_min
+        self.temp_max = temp_max
+        self.descripcion = descripcion
 
+    def mostrar_info(self):
+        return f"{self.fecha}: {self.descripcion} | Mín: {self.temp_min}°C - Máx: {self.temp_max}°C"
 
 
 
